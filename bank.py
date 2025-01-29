3494308de34cbcd697e1962e24d24a303a14dd51
teguegni <kenfackteguegni@gmail.com>
2025-01-29 04:15:56 +0100

    Update bank.py

--- bank.py
+++ bank.py
@@ -2,8 +2,8 @@
 import streamlit as st  
 import pandas as pd  
 import altair as alt  
-#import seaborn as sns  
-#import matplotlib.pyplot as plt  
+import seaborn as sns  
+import matplotlib.pyplot as plt  
 
 # Configuration de la page  
 st.set_page_config(  
